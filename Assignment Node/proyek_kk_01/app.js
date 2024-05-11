3c1ec940d58b82a7bda78cbed463112de1482f8e
Elmosius <2272008@maranatha.ac.id>
2024-05-11 13:35:44 +0700

    buat route node js

--- Assignment Node/proyek_kk_01/app.js
+++ Assignment Node/proyek_kk_01/app.js
@@ -3,36 +3,57 @@ const fs = require('fs');
 const url = require('url');
 
 let server = http.createServer((req, res) => {
-    let q = url.parse(req.url, true)
-    let path = q.query
+    let q = url.parse(req.url, true);
+    let pathname = q.pathname;
     let fileLocation;
-    switch (path.menu) {
-        case '/':
-            fileLocation = 'public/pages/dashboard.html'
-            break
-        case 'dashboard':
-            fileLocation = 'public/pages/dashboard.html'
-            break
-        case 'kk':
-            fileLocation = 'public/pages/kk/index.html'
-            break
-        case 'penduduk':
-            fileLocation = 'public/pages/penduduk/index.html'
-            break
-        default:
-            fileLocation = 'public/pages/dashboard.html'
+
+    if (pathname === '/') {
+        fileLocation = 'public/pages/login/index.html';
+    } else if (pathname.startsWith('/css') || pathname.startsWith('/js')) {
+        fileLocation = 'public' + pathname;
+    } else {
+        switch (q.query.menu) {
+            case 'dashboard':
+                fileLocation = 'public/pages/dashboard.html';
+                break;
+            case 'login':
+                fileLocation = 'public/pages/login/index.html';
+                break;
+            case 'kk':
+                fileLocation = 'public/pages/kk/index.html';
+                break;
+            case 'create-kk':
+                fileLocation = 'public/pages/kk/create-kk.html';
+                break;
+            case 'penduduk':
+                fileLocation = 'public/pages/penduduk/index.html';
+                break;
+            case 'create-penduduk':
+                fileLocation = 'public/pages/penduduk/create-penduduk.html';
+                break;
+            default:
+                fileLocation = 'public/pages/dashboard.html';
+        }
     }
 
     fs.readFile(fileLocation, (err, data) => {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'})
-            return res.end('404 Not Found')
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end('404 Not Found');
         }
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.write(data)
-        return res.end()
-    })
+        if (pathname.endsWith('.css')) {
+            res.writeHead(200, {'Content-Type': 'text/css'});
+        } else if (pathname.endsWith('.js')) {
+            res.writeHead(200, {'Content-Type': 'application/javascript'});
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+        }
 
-})
+        res.write(data);
+        return res.end();
+    });
+});
 
-server.listen(8000)
+server.listen(8000, () => {
+    console.log('Server http://localhost:8000 running');
+});
